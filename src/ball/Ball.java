2429606bbff8b2a4c8593e6b313dbe7c833e3603
lauatroannetta <lautiannetta@gmail.com>
2024-10-28 18:47:24 -0300

    Se mejoro fisica de la pelota, se agregaron bloques indesctructibles. Falta confirmar algun diseño o powerup.

---
+++ src/ball/Ball.java
@@ -0,0 +1,57 @@
+package ball;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Ball {
+
+    private int x, y;
+    private int xDir, yDir;
+    private final int diametro = 20;
+    private double velocidad = 1.0;
+
+    public Ball(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.xDir = -1;
+        this.yDir = 2;
+    }
+
+    public void mover() {
+        x += (int) (xDir * velocidad);
+        y += (int) (yDir * velocidad);
+    }
+
+    public void reverseXDir() {
+        xDir = -xDir;
+    }
+
+    public void reverseYDir() {
+        yDir = -yDir;
+    }
+
+    public void aumentarVelocidad() {
+        velocidad *= 1.1; // Aumenta más rápido
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.RED);
+        g.fillOval(x, y, diametro, diametro);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getDiametro() {
+        return diametro;
+    }
+
+    public void setxDir(int i) {
+        xDir = i;
+    }
+}
---
+++ src/generador/Generador.java
@@ -0,0 +1,47 @@
+package generador;
+
+import brick.Brick;
+
+import java.awt.*;
+
+public class Generador {
+
+    private Brick[][] mapa;
+    private int fila, columna;
+
+    int screenWidth = Toolkit.getDefaultToolkit().getScreenSize().width;
+
+    public Generador(int fila) {
+        this.fila = fila;
+        this.columna = screenWidth / 60;
+        mapa = new Brick[fila][columna];
+        int mitad = columna / 2;
+
+        for (int i = 0; i < fila; i++) {
+            for (int j = 0; j < columna; j++) {
+                // La última fila es indestructible y no tendrá bloques en el centro y en los laterales
+                if (i == fila - 1 && (j == mitad - 1 || j == mitad - 2 || j == mitad)) {
+                    mapa[i][j] = null; // Sin bloque en el centro y los adyacentes
+                } else {
+                    boolean esIndestructible = (i == fila - 1); // La última fila es indestructible
+                    mapa[i][j] = new Brick(j * 60 + 10, i * 30 + 50, 60, 30, esIndestructible);
+                }
+            }
+        }
+
+    }
+
+    public void draw(Graphics g) {
+        for (int i = 0; i < fila; i++) {
+            for (int j = 0; j < columna; j++) {
+                if (mapa[i][j] != null && mapa[i][j].isVisible()) {
+                    mapa[i][j].draw(g);
+                }
+            }
+        }
+    }
+
+    public Brick[][] getMapa() {
+        return mapa;
+    }
+}
